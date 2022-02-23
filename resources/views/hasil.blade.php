5e30c8f8ab78e829f461071b00e18cc0f1cbf349
afanfian <afanfian14@gmail.com>
2022-02-23 10:24:14 +0700

    Update desain tampilan main & hasil

--- resources/views/hasil.blade.php
+++ resources/views/hasil.blade.php
@@ -8,17 +8,18 @@
 
     <!-- bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+    {{-- Font Style --}}
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
 </head>
-
-    <div class="container">
+<body style="font-family: Poppins; font-family: fw-semibold; color:black; background-color: white ">
+    <div class="container" >
         <div class="row justify-content-center">
             <div class="col-lg-6">
                 <div class="card mt-5">
-                    <div class="card-body">
-                        <h3 style="text-align: center">Formulir Keterangan Mahasiswa Aktif</h3>
-                        <h4 class="my-4">Data Yang Sudah Di Input : </h4>
-
-                        <table class="table table-bordered table-striped">
+                    <div class="card-body" style="background-color: #37e287; border-radius: 30px; box-shadow: 10px 10px rgb(48, 47, 47);">
+                        <p style="text-align: center"><img src="/img/lambang-its.png" alt="Logo ITS" width="100px"></p>
+                        <h3 style="text-align: center; margin-bottom: 10%">Formulir Keterangan Mahasiswa Aktif</h3>
+                        <table class="table table-bordered table-striped" style="font-size: 1.2em">
                             <tr>
                                 <td style="width:150px">Nama</td>
                                 <td>{{ $data->nama }}</td>
@@ -37,22 +38,22 @@
                             </tr>
                             <tr>
                                 <td style="width:150px">Angkatan</td>
-                                <td>{{ $data->nama }}</td>
-                            </tr>
-                            <tr>
-                                <td style="width:150px">IPK</td>
-                                <td>{{ $data->ipk }}</td>
+                                <td>{{ $data->angkatan }}</td>
                             </tr>
                             <tr>
                                 <td style="width:150px">IPS</td>
                                 <td>{{ $data->ips }}</td>
                             </tr>
                             <tr>
+                                <td style="width:150px">IPK</td>
+                                <td>{{ $data->ipk }}</td>
+                            </tr>
+                            <tr>
                                 <td style="width:150px">KTM</td>
                                 <td><img src="{{ $data->ktm }}" alt="" width="200px"></td>
                             </tr>
                         </table>
-                        <a href="/main" class="btn btn-success">Kembali</a>
+                        <a href="/main" class="btn btn-primary" style="text-align: center" >Kembali</a>
                     </div>
                 </div>
             </div>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -8,28 +8,30 @@
  
     <!-- bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+    {{-- Font Style --}}
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
 </head>
-<body>
+<body style="font-family: Poppins; font-family: fw-semibold; color:black; background-color: white ">
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-lg-6">
                     <div class="card mt-5">
-                        <div class="card-body">
-                            <h3 class="text-center">Formulir Keterangan Mahasiswa Aktif</h3>
+                        <div class="card-body" style="background-color: #37e287; border-radius: 30px; box-shadow: 10px 10px rgb(48, 47, 47);">
+                            <p style="text-align: center"><img src="/img/lambang-its.png" alt="Logo ITS" width="100px"></p>
+                            <h3 class="text-center">Formulir Keterangan Mahasiswa Aktif ITS</h3>
                             <br/>
-
-                            @if (count($errors) > 0)
-                            <div class="alert alert-danger">
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                            @endif
+                                @if (count($errors) > 0)
+                                <div class="alert alert-danger">
+                                    <ul>
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                                @endif
                             <br/>
                              <!-- form hasil -->
-                            <form action="/hasil" method="post" enctype="multipart/form-data">
+                            <form action="/hasil" method="post" enctype="multipart/form-data" style="font-size: 1.2em">
                                 {{ csrf_field() }}
                                 <div class="form-group">
                                     <label for="nama">Nama</label>
@@ -52,19 +54,19 @@
                                     <input class="form-control" type="text" name="angkatan" value="{{ old('angkatan') }}">
                                 </div>
                                 <div class="form-group">
-                                    <label for="ipk">IPK</label>
-                                    <input class="form-control" type="text" name="ipk" value="{{ old('ipk') }}">
-                                </div>
-                                <div class="form-group">
                                     <label for="ips">IPS</label>
                                     <input class="form-control" type="text" name="ips" value="{{ old('ips') }}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="ipk">IPK</label>
+                                    <input class="form-control" type="text" name="ipk" value="{{ old('ipk') }}">
                                 </div>
                                 <div class="form-group">
                                     <label for="foto">KTM</label>
                                     <input type="file" class="form-control-file" id="ktm" name="ktm" accept="image/png">
                                 </div>
-                                <div class="form-group">
-                                    <input class="btn btn-primary" type="submit" value="Proses">
+                                <div class="form-group" style="text-align: center">
+                                    <input class="btn btn-primary" type="submit" value="Kirim" >
                                 </div>
                             </form>
  
