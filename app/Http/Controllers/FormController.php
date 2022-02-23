dd961859c28aef30dcdd8a10579a94997d9f212d
afanfian <afanfian14@gmail.com>
2022-02-23 07:13:21 +0700

    Create view image

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -28,5 +28,33 @@ class FormController extends Controller
         ]);
         return view('hasil',['data' => $request]);
     }
-    
+    public function create()
+    {
+        return view('main');
+    }
+
+    public function store(Request $request)
+    {
+        $post = Post::create([
+            'caption' => $request->caption,
+            'id_user' => Auth::guard('web')->user()->id
+        ]);
+        $post->update([
+            'foto' => $this->saveFoto($request, $post->id)
+        ]);
+
+        return redirect()->route('post.view', ['id' => $post->id]);
+    }
+
+    public function saveFoto(Request $request, $id)
+    {
+        $foto = $request->foto; // typedata : file
+        $foto_name = ''; // typedata : string
+        if ($foto !== NULL) {
+            $foto_name = 'foto' . '-' . $id . "." . $foto->extension(); // typedata : string
+            $foto_name = str_replace(' ', '-', strtolower($foto_name)); // typedata : string
+            $foto->storeAs(null, $foto_name, ['disk' => 'public']); // memanggil function untuk menaruh file di storage
+        }
+        return asset('storage') . '/' . $foto_name; // me return path/to/file.ext
+    }
 }
